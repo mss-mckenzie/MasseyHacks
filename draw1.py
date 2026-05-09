01cabcca5306240fcdb2f6aa6db73ef033b5c6d7
Ron McKenzie <ronmcke@gmail.com>
2026-05-09 11:02:09 -0400

    Update draw1.py

--- draw1.py
+++ draw1.py
@@ -1,14 +1,9 @@
 from mckPy import *
 
-c = "beige"
 while running():
-    fill(c)
-        
+    fill("beige")       
     drawRect("red", 100, 50, 200, 50, outline = 3)
-    if overRect(100, 50, 200, 50):
-        drawRect("red3", 100, 50, 200, 50, outline = 3)
-    if clickOnRect(100, 50, 200, 50):
-        c = "tan"
+
         
 
         
